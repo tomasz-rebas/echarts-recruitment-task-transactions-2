72ce0ef4a5ae4673c760659b57b8eec66658dc78
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2024-09-06 16:04:40 +0200

    Remove a comment

--- src/types.ts
+++ src/types.ts
@@ -12,19 +12,3 @@ export interface ErrorResponse {
   code: number;
   msg: string;
 }
-
-/*
-
-[
-  {
-    "id": 28457,
-    "price": "4.00000100",
-    "qty": "12.00000000",
-    "quoteQty": "48.000012",
-    "time": 1499865549590,
-    "isBuyerMaker": true,
-    "isBestMatch": true
-  }
-]
-
-*/
