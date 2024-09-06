858be85e41690e68332aeb899dbeed9dc69cdbd4
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2024-09-06 16:42:52 +0200

    Separate rendering case for empty array

--- src/TransactionChart.tsx
+++ src/TransactionChart.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { Chart } from "./Chart";
-import { ErrorResponse, Trade } from "./types";
+import { Trade } from "./types";
 
 export const TransactionChart = () => {
   const [dataset, setDataset] = useState<Trade[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>();
+  const [error, setError] = useState<string | null>(null);
 
   const apiUrl = "https://api.binance.com/api/v3/trades?symbol=BTCUSDT";
 
@@ -14,7 +14,6 @@ export const TransactionChart = () => {
       const response = await fetch(apiUrl);
 
       if (!response.ok) {
-        console.log("response", response);
         throw new Error("Couldn't fetch the data");
       }
 
@@ -43,7 +42,11 @@ export const TransactionChart = () => {
     return <h2>Loading...</h2>;
   }
 
-  if (error || dataset.length === 0) {
+  if (dataset.length === 0) {
+    return <h2>No data has been fetched. The chart cannot be displayed.</h2>;
+  }
+
+  if (error) {
     return <h2>Error occurred. The chart cannot be displayed. {error}</h2>;
   }
 
